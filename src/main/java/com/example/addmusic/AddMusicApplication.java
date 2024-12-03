d175a1e8eb304a46daf9334a01d0f0503568087e
ms010209 <129668058+ms010209@users.noreply.github.com>
2024-12-03 16:13:57 +0900

    Add .gitignore file

---
+++ src/main/java/com/example/addmusic/AddMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.addmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/addmusic/AddMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
